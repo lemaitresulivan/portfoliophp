21c7579b9eaee092bd2f2c94a8d283f20718450f
lemaitresulivan <sulidu44@live.fr>
2021-03-19 20:11:34 +0100

    header, footer et index initialisés

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
 
 // Point d'ancrage pour la HomePage
-// TODO : Insérer les ``` require__DIR__.'/templates/...'; ```
+// TODO : Insérer les ``` require__DIR__.'/templates/...'; ```  TODO = OK.
+require __DIR__.'/templates/header.tpl.php';
+require __DIR__.'/templates/home.tpl.php';
+require __DIR__.'/templates/footer.tpl.php';
 
 ?>
 
---
+++ templates/header.tpl.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="Développeur web, Développement, junior, HTML, CSS, PHP, Javascript, Symfony">
+    <meta name="description" content="Portfolio de Sulivan Le Maitre, développeur web junior. Disponible pour une première prise de poste.">
+    <meta name="author" content="Sulivan Le Maitre">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sulivan Le Maitre - Développeur web Junior - Portfolio</title>
+    <link rel="stylesheet" href="src/css/style.css">
+</head>
+<body>
+    <main>
+    
+
